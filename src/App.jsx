dc79e3a7a2bb27102215f51af38382ee2ddaba11
AshBuilder <ash@prah.dev>
2025-05-25 16:27:22 +0300

    fix: удалён BrowserRouter из App для поддержки MemoryRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Welcome from './screens/Welcome';
 import Init from './screens/Init';
 import Path from './screens/Path';
@@ -8,13 +8,11 @@ import Profile from './screens/Profile';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/init" element={<Init />} />
-        <Route path="/path" element={<Path />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/init" element={<Init />} />
+      <Route path="/path" element={<Path />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
