c1f0651f60032e60515cd5cfb1dfeae61f4284de
AshBuilder <ash@prah.dev>
2025-05-27 22:10:03 +0300

    Добавлен редирект с / на /test для маршрутов

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Welcome from './screens/Welcome';
 import Init from './screens/Init';
@@ -9,10 +9,24 @@ export default function App() {
   return (
     <Routes>
       <Route path="/test" element={<Init />} />
-      {/* <Route path="/" element={<Welcome />} />
-      {/* <Route path="/init" element={<Init />} /> */}
-      {/* <Route path="/path" element={<Path />} />
-      <Route path="/profile" element={<Profile />} /> */} 
+      <Route path="/" element={<Welcome />} />
+      <Route path="/init" element={<Init />} /> 
+      <Route path="/path" element={<Path />} />
+      <Route path="/profile" element={<Profile />} /> 
     </Routes>
+  );
+}
+ */
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Init from './Init';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/test" element={<Init />} />
+        <Route path="/" element={<Navigate to="/test" />} />
+      </Routes>
+    </Router>
   );
 }
