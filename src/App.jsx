ddd7364b54d196cea27d5dd2290a50ec1419e9e3
AshBuilder <ash@prah.dev>
2025-05-30 15:02:46 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Welcome from './screens/Welcome';
-import Init from './screens/Init';
-import Path from './screens/Path';
-import Profile from './screens/Profile';
-import Final from './screens/Final';
+import Init     from './screens/Init';
+import Path     from './screens/Path';
+import Profile  from './screens/Profile';
+import Final    from './screens/Final';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/init" element={<Init />} />
-        <Route path="/path" element={<Path />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/final" element={<Final />} />
-        {/* все остальные ведут на главную */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/"        element={<Welcome />} />
+      <Route path="/init"    element={<Init />} />
+      <Route path="/path"    element={<Path />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/final"   element={<Final />} />
+      <Route path="*"        element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
+// src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
