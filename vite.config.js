394fdfd56aa5d9d25f6c57f2c75f38db7dc5bad3
AshBuilder <ash@prah.dev>
2025-07-01 13:45:21 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['ton-inpage-provider']
+  },
+  // Для SSR (если у вас используется) — не выносить из бандла
+  ssr: {
+    noExternal: ['ton-inpage-provider']
+  },
 });
