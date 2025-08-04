19cd1d4e4016e70a1e672897b13e49e85c2fe5ba
AshBuilder <ash@prah.dev>
2025-08-04 15:46:37 +0300

    2

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,18 +3,38 @@ import bgWelcome from '../assets/images/converted_minimal.jpg';
 
 export default function Home() {
   return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        backgroundImage: `url(${bgWelcome})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-    />
+    <>
+      {/* Обёртка для фонового изображения и оверлея */}
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          overflow: 'hidden',
+        }}
+      >
+        {/* 1) Фоновый слой */}
+        <div
+          style={{
+            position: 'absolute',
+            inset: 0,
+            backgroundImage: `url(${bgWelcome})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+        {/* 2) Полупрозрачный чёрный оверлей */}
+        <div
+          style={{
+            position: 'absolute',
+            inset: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.6)',
+          }}
+        />
+      </div>
+    </>
   );
 }
