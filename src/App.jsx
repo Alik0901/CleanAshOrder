eca2f1612d6d9468654586fb9f1f8e60f937d56d
AshBuilder <ash@prah.dev>
2025-08-11 23:14:49 +0300

    -2

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,14 @@ import Leaderboard from './screens/Leaderboard';
 import FinalPhrase from './screens/FinalPhrase';
 import Congrats    from './screens/Congrats';
 import Profile     from './screens/Profile';
+import ThirdQuest  from './screens/ThirdQuest';
 
 import { AuthContext } from './context/AuthContext';
 
 export default function App() {
   const { user } = useContext(AuthContext);
 
-  // 1) Показываем заглушку, пока проверяем сессию
+  // Пока инициализируем сессию
   if (user === undefined) {
     return (
       <div style={{ padding: 20, textAlign: 'center' }}>
@@ -32,87 +33,51 @@ export default function App() {
       {/* Login */}
       <Route
         path="/login"
-        element={
-          user
-            ? <Navigate to="/" replace />
-            : <Login />
-        }
+        element={ user ? <Navigate to="/" replace /> : <Login /> }
       />
 
       {/* Защищённые маршруты */}
       <Route
         path="/"
-        element={
-          user
-            ? <Home />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Home /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/burn"
-        element={
-          user
-            ? <Burn />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Burn /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/gallery"
-        element={
-          user
-            ? <Gallery />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Gallery /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/referral"
-        element={
-          user
-            ? <Referral />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Referral /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/leaderboard"
-        element={
-          user
-            ? <Leaderboard />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Leaderboard /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/final"
-        element={
-          user
-            ? <FinalPhrase />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <FinalPhrase /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/congrats"
-        element={
-          user
-            ? <Congrats />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Congrats /> : <Navigate to="/login" replace /> }
       />
       <Route
         path="/profile"
-        element={
-          user
-            ? <Profile />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Profile /> : <Navigate to="/login" replace /> }
+      />
+      <Route
+        path="/third"
+        element={ user ? <ThirdQuest /> : <Navigate to="/login" replace /> }
       />
 
-      {/* Любой другой путь */}
+      {/* Фоллбек */}
       <Route
         path="*"
-        element={
-          user
-            ? <Navigate to="/" replace />
-            : <Navigate to="/login" replace />
-        }
+        element={ user ? <Navigate to="/" replace /> : <Navigate to="/login" replace /> }
       />
     </Routes>
   );
